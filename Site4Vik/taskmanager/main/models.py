f8bb778227c2b26b3382eabf42d1edbd47669196
AlexXXll <55854752+AlexXXll@users.noreply.github.com>
2020-10-22 15:01:56 +0400

    Новая фича

--- Site4Vik/taskmanager/main/models.py
+++ Site4Vik/taskmanager/main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.TextField('Описание')
+    image = models.ImageField(blank=True, upload_to='images/blog/%Y/%m/%d', help_text='150x150px', verbose_name='Ссылка картинки')
 
     def __str__(self):
         return self.title
